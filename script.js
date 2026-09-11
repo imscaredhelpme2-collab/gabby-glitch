d6f3a310b3ade03dc9f0b6e8e28addbae0a109e8
imscaredhelpme2-collab <imscaredhelpme2@gmail.com>
2026-09-11 23:20:55 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
-let messages = [
+const glitches = [
+    {
+        name: "MEMORY_FRAGMENT",
+        message: "I found something I don't remember creating."
+    },
 
-    "RECOVERING MEMORY...",
-    "SEARCHING...",
-    "UNKNOWN PROCESS RUNNING...",
-    "GABBY ONLINE",
-    "ERROR DETECTED"
+    {
+        name: "SYSTEM_DESYNC",
+        message: "Something feels slightly out of sync."
+    },
 
-    ];
+    {
+        name: "DREAM_MODE",
+        message: "I think I was dreaming."
+    },
+
+    {
+        name: "UNKNOWN_SIGNAL",
+        message: "A signal appeared from nowhere."
+    },
+
+    {
+        name: "CORRUPTED_MEMORY",
+        message: "A memory file was damaged."
+    }
+];
+
+
+let glitch = glitches[Math.floor(Math.random() * glitches.length)];
+
+
+document.getElementById("glitchName").innerHTML =
+glitch.name;
+
+
+document.getElementById("glitchMessage").innerHTML =
+glitch.message;
+
+
+document.getElementById("time").innerHTML =
+new Date();
 
 
     setInterval(function(){
